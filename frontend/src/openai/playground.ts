72ea71ca9e2e3cae2d58999925528f1f26a7ef3e
sutigit <panyanilben@gmail.com>
2025-04-06 20:49:04 +0300

    update room api

--- frontend/src/openai/playground.ts
+++ frontend/src/openai/playground.ts
@@ -27,33 +27,37 @@ function printCommands() {
   console.log("aar - add assistant to room");
   console.log("rar - remove assistant from room");
   console.log("");
-  console.log("atr - add discussion topic to room");
+  console.log("urt - add discussion topic to room");
+  console.log("drt - delete discussion topic in room");
   console.log("");
   console.log("mpr - modify rooms turn policy");
   console.log("mnt - modify rooms number of turns");
   console.log("mds - modify discussion starter");
   console.log("");
+  console.log("sdr - show discussion in room");
+  console.log("sr - start discussion in room");
+  console.log("");
   console.log("rrt - reset rooms discussion thread");
   console.log("rr - reset room");
   console.log("");
 
-  // console.log("LEGACY #########################");
-  // console.log("Threads 🧵 ------------------------");
-  // console.log("lt - list all threads");
-  // console.log("ct - create thread");
-  // console.log("dt - delete thread");
-  // console.log("");
+  console.log("Threads 🧵 ------------------------");
+  console.log("lt - list all threads");
+  console.log("ct - create thread");
+  console.log("dt - delete thread");
+  console.log("");
 
-  // console.log("Messaging 💬 ----------------------");
-  // console.log("am - add message to thread");
-  // console.log("gm - get messages from thread");
-  // console.log("");
+  console.log("Messaging 💬 ----------------------");
+  console.log("am - add message to thread");
+  console.log("gm - get messages from thread");
+  console.log("");
 
-  // console.log("Runs ▶️ ---------------------------");
-  // console.log("rt - run thread");
-  // console.log("");
+  console.log("Runs ▶️ ---------------------------");
+  console.log("rt - run thread");
+  console.log("");
 
-  console.log("Quit ❌ ---------------------------");
+  console.log("Playground 🚀 ---------------------------");
+  console.log("lc - list commands");
   console.log("q - quit");
   console.log("");
 }
@@ -109,89 +113,183 @@ async function command() {
 
       // DELETE ROOM
       case "dr":
-        rl.question("Room ID: ", (roomId) => {
-          (async () => {
-            const deletedRoom: Room | false = await room.deleteRoom(roomId);
-            if (deletedRoom) {
-              console.log("Room deleted successfully!");
-            } else {
-              console.log("Failed to delete room");
-            }
-            console.log("");
-            command();
-          })();
+        rl.question("Room ID: ", async (roomId) => {
+          const deletedRoom: Room | false = await room.deleteRoom(roomId);
+          if (deletedRoom) {
+            console.log("Room deleted successfully!");
+          } else {
+            console.log("Failed to delete room");
+          }
+          console.log("");
+          command();
         });
         break;
 
       // INSPECT ROOM
       case "ir":
-        rl.question("Room ID: ", (roomId) => {
-          (async () => {
-            const roomData: Room | false = room.inspectRoom(roomId);
-            if (roomData) {
-              console.log("---------------------");
-              console.log("Room ID:", roomData.id);
-              console.log("Description:", roomData.description);
-              console.log("Thread ID:", roomData.threadId);
-              console.log("Number of assistants:", roomData.assistants.length);
-              console.log("Assistants in room:");
-              roomData.assistants.forEach((assistantId: string) => {
-                console.log(`Assistant ID: ${assistantId}`);
-              });
-              console.log("Turn Policy:", roomData.turnPolicy);
-              console.log("Turn Limit:", roomData.turnLimit);
-              console.log("Discussion Starter:", roomData.discussionStarter);
-              console.log("---------------------");
-            } else {
-              console.log("Failed to inspect room");
-            }
-            console.log("");
-            command();
-          })();
+        rl.question("Room ID: ", async (roomId) => {
+          const roomData: Room | false = room.inspectRoom(roomId);
+          if (roomData) {
+            console.log("---------------------");
+            console.log("Room ID:", roomData.id);
+            console.log("Description:", roomData.description);
+            console.log("Thread ID:", roomData.threadId);
+            console.log("Number of assistants:", roomData.assistants.length);
+            console.log("Assistants in room:");
+            roomData.assistants.forEach((assistantId: string) => {
+              console.log(`Assistant ID: ${assistantId}`);
+            });
+            console.log("Turn Policy:", roomData.turnPolicy);
+            console.log("Turn Limit:", roomData.turnLimit);
+            console.log("Discussion Starter:", roomData.discussionStarter);
+            console.log("Discussion Topic:", roomData.discussionTopic);
+            console.log("---------------------");
+          } else {
+            console.log("Failed to inspect room");
+          }
+          console.log("");
+          command();
         });
         break;
 
       // LIST ASSISTANTS IN ROOM
       case "lar":
-        rl.question("Room ID: ", (roomId) => {
-          (async () => {
-            const assistants: string[] | false =
-              room.getAssistantsInRoom(roomId);
+        rl.question("Room ID: ", async (roomId) => {
+          const assistants: string[] | false = room.getAssistantsInRoom(roomId);
 
-            if (assistants && assistants.length === 0) {
-              console.log("No assistants in room");
-            } else if (assistants && assistants.length > 0) {
-              console.log("Assistants in room:");
-              assistants.forEach((assistantId: string) => {
-                console.log(`Assistant ID: ${assistantId}`);
-              });
-            } else {
-              console.log("Failed to get assistants in room");
-            }
-            console.log("");
-            command();
-          })();
+          if (assistants && assistants.length === 0) {
+            console.log("No assistants in room");
+          } else if (assistants && assistants.length > 0) {
+            console.log("Assistants in room:");
+            assistants.forEach((assistantId: string) => {
+              console.log(`Assistant ID: ${assistantId}`);
+            });
+          } else {
+            console.log("Failed to get assistants in room");
+          }
+          console.log("");
+          command();
         });
         break;
 
       // ADD ASSISTANT TO ROOM
       case "aar":
         rl.question("Room ID: ", (roomId) => {
-          rl.question("Assistant ID: ", (assistantId) => {
-            (async () => {
-              const updatedRoom: Room | false = await room.addAssistantToRoom(
-                roomId,
-                assistantId
+          rl.question("Assistant ID: ", async (assistantId) => {
+            const updatedRoom: Room | false = await room.addAssistantToRoom(
+              roomId,
+              assistantId
+            );
+            if (updatedRoom) {
+              console.log("Assistant added to room successfully!");
+            } else {
+              console.log("Failed to add assistant to room");
+            }
+            console.log("");
+            command();
+          });
+        });
+        break;
+
+      // ADD DISCUSSION TOPIC TO ROOM
+      case "urt":
+        rl.question("Room ID: ", (roomId) => {
+          rl.question("Discussion Topic: ", (topic) => {
+            const updatedRoom: Room | false = room.updateRoomTopic(
+              roomId,
+              topic
+            );
+            if (updatedRoom) {
+              console.log("Discussion topic added to room successfully!");
+            } else {
+              console.log("Failed to add discussion topic to room");
+            }
+            console.log("");
+            command();
+          });
+        });
+        break;
+
+      // DELETE DISCUSSION TOPIC IN ROOM
+      case "drt":
+        rl.question("Room ID: ", (roomId) => {
+          const updatedRoom: Room | false = room.deleteRoomTopic(roomId);
+          if (updatedRoom) {
+            console.log("Discussion topic deleted successfully!");
+          } else {
+            console.log("Failed to delete discussion topic");
+          }
+          console.log("");
+          command();
+        });
+        break;
+
+      // SHOW DISCUSSION IN ROOM
+      case "sdr":
+        rl.question("Room ID: ", async (roomId) => {
+          const discussion = await room.getDiscussionInRoom(roomId);
+          if (discussion) {
+            console.log("Discussion:");
+            discussion.forEach((message) => {
+              console.log(
+                `>> Role: ${message.role}, Message ID: ${message.id}, Assistant ID: ${message.assistantId}`
               );
-              if (updatedRoom) {
-                console.log("Assistant added to room successfully!");
+              console.log("----------------------");
+              if ("text" in message.content[0]) {
+                console.log(message.content[0].text.value);
               } else {
-                console.log("Failed to add assistant to room");
+                console.log("Message content is not text-based.");
               }
               console.log("");
-              command();
-            })();
-          });
+            });
+            console.log("");
+          } else {
+            console.log("Failed to show discussion in room");
+          }
+          command();
+        });
+        break;
+
+      // START DISCUSSION IN ROOM
+      case "sr":
+        rl.question("Room ID: ", async (roomId) => {
+          const discussion = await room.startDiscussionInRoom(roomId);
+          console.log("");
+          if (discussion) {
+            console.log("Discussion ended successfully!");
+          } else {
+            console.log("Discussion failed");
+          }
+          console.log("");
+          command();
+        });
+        break;
+
+      // RESET ROOM
+      case "rr":
+        rl.question("Room ID: ", async (roomId) => {
+          const success = await room.resetRoom(roomId);
+          if (success) {
+            console.log("Room reset successfully!");
+          } else {
+            console.log("Failed to reset room");
+          }
+          console.log("");
+          command();
+        });
+        break;
+
+      // RESET ROOMS DISCUSSION THREAD
+      case "rrt":
+        rl.question("Room ID: ", async (roomId) => {
+          const success = await room.resetRoomThread(roomId);
+          if (success) {
+            console.log("Room thread reset successfully!");
+          } else {
+            console.log("Failed to reset room thread");
+          }
+          console.log("");
+          command();
         });
         break;
 
@@ -223,17 +321,15 @@ async function command() {
 
       // DELETE THREAD
       case "dt":
-        rl.question("Thread ID: ", (threadId) => {
-          (async () => {
-            const success = await sdk.deleteThread(threadId);
-            if (success) {
-              console.log("Thread deleted from threads.json successfully!");
-            } else {
-              console.log("Failed to delete thread from threads.json");
-            }
-            console.log("");
-            command();
-          })();
+        rl.question("Thread ID: ", async (threadId) => {
+          const success = await sdk.deleteThread(threadId);
+          if (success) {
+            console.log("Thread deleted from threads.json successfully!");
+          } else {
+            console.log("Failed to delete thread from threads.json");
+          }
+          console.log("");
+          command();
         });
         break;
 
@@ -241,45 +337,41 @@ async function command() {
       // ADD MESSAGE TO THREAD
       case "am":
         rl.question("Thread ID: ", (threadId) => {
-          rl.question("Message: ", (message) => {
-            (async () => {
-              const success = await sdk.addMessageToThread(threadId, message);
-              if (success) {
-                console.log("Message added to thread successfully!");
-              } else {
-                console.log("Failed to add message to thread");
-              }
-              console.log("");
-              command();
-            })();
+          rl.question("Message: ", async (message) => {
+            const success = await sdk.addMessageToThread(threadId, message);
+            if (success) {
+              console.log("Message added to thread successfully!");
+            } else {
+              console.log("Failed to add message to thread");
+            }
+            console.log("");
+            command();
           });
         });
         break;
 
       // GET MESSAGES FROM THREAD
       case "gm":
-        rl.question("Thread ID: ", (threadId) => {
-          (async () => {
-            const messages = await sdk.getMessagesFromThread(threadId);
-            if (messages) {
-              messages.forEach((message) => {
-                console.log(
-                  `>> Role: ${message.role}, Message ID: ${message.id}, Assistant ID: ${message.assistantId}`
-                );
-                console.log("----------------------");
-                if ("text" in message.content[0]) {
-                  console.log(message.content[0].text.value);
-                } else {
-                  console.log("Message content is not text-based.");
-                }
-                console.log("");
-              });
+        rl.question("Thread ID: ", async (threadId) => {
+          const messages = await sdk.getMessagesFromThread(threadId);
+          if (messages) {
+            messages.forEach((message) => {
+              console.log(
+                `>> Role: ${message.role}, Message ID: ${message.id}, Assistant ID: ${message.assistantId}`
+              );
+              console.log("----------------------");
+              if ("text" in message.content[0]) {
+                console.log(message.content[0].text.value);
+              } else {
+                console.log("Message content is not text-based.");
+              }
               console.log("");
-            } else {
-              console.log("Failed to get messages from thread");
-            }
-            command();
-          })();
+            });
+            console.log("");
+          } else {
+            console.log("Failed to get messages from thread");
+          }
+          command();
         });
         break;
 
@@ -316,6 +408,12 @@ async function command() {
         break;
 
       // PLAYGROUND COMMANDS -----------------------------------------------------------------------------------
+      // LIST COMMANDS
+      case "lc":
+        printCommands();
+        command();
+        break;
+
       // QUIT
       case "q":
         rl.close();
--- frontend/src/openai/room.ts
+++ frontend/src/openai/room.ts
@@ -12,6 +12,7 @@ export interface Room {
   turnPolicy: "random" | "sequential";
   turnLimit: number;
   discussionStarter: number;
+  discussionTopic: string | null;
 }
 
 export default class RoomAPI {
@@ -24,7 +25,7 @@ export default class RoomAPI {
   constructor() {
     this.path = "./src/openai/data/rooms.json";
     this.maxAssistants = 4;
-    this.defaultTurnLimit = 4;
+    this.defaultTurnLimit = 3;
     this.defaultTurnPolicy = "sequential";
     this.defaultDiscussionStarter = 0;
   }
@@ -66,6 +67,7 @@ export default class RoomAPI {
       turnPolicy: this.defaultTurnPolicy,
       turnLimit: this.defaultTurnLimit,
       discussionStarter: this.defaultDiscussionStarter,
+      discussionTopic: null,
     };
 
     let existingData = [];
@@ -154,7 +156,15 @@ export default class RoomAPI {
 
   private updateRoom(
     roomId: string,
-    newRoomData: { assistants?: string[]; threadId?: string }
+    newRoomData: {
+      threadId?: string;
+      assistants?: string[];
+      description?: string;
+      turnPolicy?: string;
+      turnLimit?: number;
+      discussionStarter?: number;
+      discussionTopic?: string | null;
+    }
   ) {
     const rooms = this.getRooms();
     const roomIndex = rooms.findIndex((room) => room.id === roomId);
@@ -224,4 +234,257 @@ export default class RoomAPI {
   }
 
   removeAssistantFromRoom(roomId: string, assistantId: string) {}
+
+  updateRoomTopic(roomId: string, topic: string) {
+    const room = this.inspectRoom(roomId);
+    if (!room) {
+      console.error("Room not found");
+      return false;
+    }
+
+    // Update room in the file
+    const updatedRoom = this.updateRoom(roomId, {
+      discussionTopic: topic,
+    });
+
+    if (!updatedRoom) {
+      console.error("Failed to update room");
+      return false;
+    }
+
+    return updatedRoom;
+  }
+
+  deleteRoomTopic(roomId: string) {
+    const room = this.inspectRoom(roomId);
+    if (!room) {
+      console.error("Room not found");
+      return false;
+    }
+
+    // Update room in the file
+    const updatedRoom = this.updateRoom(roomId, {
+      discussionTopic: null,
+    });
+
+    if (!updatedRoom) {
+      console.error("Failed to update room");
+      return false;
+    }
+
+    return updatedRoom;
+  }
+
+  async startDiscussionInRoom(roomId: string) {
+    const room = this.inspectRoom(roomId);
+    if (!room) {
+      console.error("Room not found");
+      return false;
+    }
+
+    if (!room.threadId) {
+      console.error("Room is no longer valid");
+      return false;
+    }
+
+    if (room.assistants.length === 0) {
+      console.error("Must have at least one assistant in the room");
+      return false;
+    }
+
+    if (room.discussionTopic === null) {
+      console.error("Must have a discussion topic");
+      return false;
+    }
+
+    if (room.turnLimit <= 0) {
+      console.error("Turn limit must be greater than 0");
+      return false;
+    }
+
+    if (
+      room.discussionStarter < 0 ||
+      room.discussionStarter >= room.assistants.length
+    ) {
+      console.error("Invalid discussion starter");
+      return false;
+    }
+
+    // Add the topic to thread
+    const message = await osdk.addMessageToThread(
+      room.threadId,
+      room.discussionTopic
+    );
+    if (!message) {
+      console.error(
+        "Failed to start discussion because failed to add topic to thread, please try again"
+      );
+      return false;
+    }
+
+    // Start discussion
+    let currentTurn: number = 0;
+    let assistantTurn: number = room.discussionStarter;
+    let selectedAssistantId: string = room.assistants[assistantTurn];
+
+    while (currentTurn < room.turnLimit) {
+      // Let everybody speak per turn
+      for (let i = 0; i < room.assistants.length; i++) {
+        try {
+          await this.streamRun(room.threadId, selectedAssistantId);
+        } catch (err) {
+          console.error("Error in stream run:", err);
+          return false; // Stop the discussion on error
+        }
+
+        // Determine the next assistant to speak
+        if (room.turnPolicy === "sequential") {
+          assistantTurn = (assistantTurn + 1) % room.assistants.length;
+        } else if (room.turnPolicy === "random") {
+          // Todo: implement random turn policy
+        }
+
+        // Assign next assistant to speak
+        selectedAssistantId = room.assistants[assistantTurn];
+      }
+
+      currentTurn++;
+    }
+
+    return true;
+  }
+
+  private async streamRun(
+    threadId: string,
+    assistantId: string
+  ): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      const run = osdk.createRun(threadId, assistantId);
+
+      if (run) {
+        run
+          .on("textCreated", (text) =>
+            process.stdout.write(`\n>> Assistant ID: ${assistantId}\n`)
+          )
+          .on("textDelta", (textDelta, snapshot) =>
+            process.stdout.write(textDelta.value || "")
+          )
+          .on("end", () => {
+            process.stdout.write("\n");
+            resolve(); // resolve the promise on end
+          })
+          .on("error", (err) => {
+            console.error("Error in run:", err);
+            process.stdout.write("\n");
+            reject(err); // reject the promise on error
+          });
+      } else {
+        console.error("Failed to create run");
+        reject(new Error("Failed to create run")); // reject if run is not created
+      }
+    });
+  }
+
+  async getDiscussionInRoom(roomId: string) {
+    const room = this.inspectRoom(roomId);
+    if (!room) {
+      console.error("Room not found");
+      return false;
+    }
+    if (!room.threadId) {
+      console.error("Room is no longer valid");
+      return false;
+    }
+
+    const messages = await osdk.getMessagesFromThread(room.threadId);
+    if (!messages) {
+      console.error("Failed to get messages from thread");
+      return false;
+    }
+
+    return messages;
+  }
+
+  async resetRoom(roomId: string) {
+    const room = this.inspectRoom(roomId);
+    if (!room) {
+      console.error("Room not found");
+      return false;
+    }
+
+    // Create new thread
+    const newThread = await osdk.createThread();
+    if (!newThread) {
+      console.error("Failed to create new thread, please try again");
+      return false;
+    }
+    const newThreadId = newThread.id;
+
+    // Reset room in the file
+    const updatedRoom = this.updateRoom(roomId, {
+      threadId: newThreadId,
+      assistants: [],
+      description: "New Discussion",
+      turnPolicy: this.defaultTurnPolicy,
+      turnLimit: this.defaultTurnLimit,
+      discussionStarter: this.defaultDiscussionStarter,
+      discussionTopic: null,
+    });
+
+    if (!updatedRoom) {
+      console.error("Failed to reset room");
+      await osdk.deleteThread(newThreadId);
+      return false;
+    }
+
+    // Delete the old thread
+    try {
+      await osdk.deleteThread(room.threadId);
+      console.log("Old thread deleted successfully!");
+    } catch (err) {
+      console.error("Error deleting old thread:", err);
+      return false;
+    }
+
+    return updatedRoom;
+  }
+
+  async resetRoomThread(roomId: string) {
+    const room = this.inspectRoom(roomId);
+    if (!room) {
+      console.error("Room not found");
+      return false;
+    }
+
+    // Create new thread
+    const newThread = await osdk.createThread();
+    if (!newThread) {
+      console.error("Failed to create new thread, please try again");
+      return false;
+    }
+    const newThreadId = newThread.id;
+
+    // Reset room in the file
+    const updatedRoom = this.updateRoom(roomId, {
+      threadId: newThreadId,
+      discussionTopic: null,
+    });
+
+    if (!updatedRoom) {
+      console.error("Failed to reset room");
+      await osdk.deleteThread(newThreadId);
+      return false;
+    }
+
+    // Delete the old thread
+    try {
+      await osdk.deleteThread(room.threadId);
+      console.log("Old thread deleted successfully!");
+    } catch (err) {
+      console.error("Error deleting old thread:", err);
+      return false;
+    }
+
+    return updatedRoom;
+  }
 }
